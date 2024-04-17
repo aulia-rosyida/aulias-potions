6acbc83b01be334ef31ae747f519107d2f816531
aulia-rosyida <rosyidaaulia24@gmail.com>
2024-04-17 17:27:56 +0700

    feat: url launcher demo 6.2.4 version

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:aulias_potions/src/demo_url_launcher/url_launcher_page.dart';
 import 'package:flutter/material.dart';
 
 import 'src/app.dart';
@@ -16,5 +17,22 @@ void main() async {
   // Run the app and pass in the SettingsController. The app listens to the
   // SettingsController for changes, then passes it further down to the
   // SettingsView.
-  runApp(AuliaApp(settingsController: settingsController));
+  // runApp(AuliaApp(settingsController: settingsController));
+
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'URL Launcher',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'URL Launcher'),
+    );
+  }
 }
